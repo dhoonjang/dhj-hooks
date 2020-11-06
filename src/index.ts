c0064b2fd2f7936316e26688671f032af261a6dd
dhoonjang <dhoonjang@gmail.com>
2020-11-06 12:05:30 +0900

    feat: update fetch hook

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,34 @@ export interface CustomEventListenerOptions extends AddEventListenerOptions {
   initExecute?: boolean;
 }
 
+export interface IApiReturn {
+  success: boolean;
+  error?: any;
+}
+
+export function useUpdateFetch<T extends IApiReturn>(
+  request: () => Promise<T>,
+  maxUpdate?: number
+): [T | null, (updateNum?: number) => void] {
+  const [update, setUpdate] = useState<number>(1);
+  const [res, setRes] = useState<T | null>(null);
+
+  useEffect(() => {
+    const requestFunc = async () => {
+      const response = await request();
+      if (response.success) setRes(response);
+    };
+    if (update > 0 && (!maxUpdate || maxUpdate > update)) requestFunc();
+  }, [update, request, maxUpdate]);
+
+  const updateFunc = useCallback(
+    (updateNum?: number) => setUpdate(updateNum ? updateNum : update + 1),
+    [update]
+  );
+
+  return [res, updateFunc];
+}
+
 export function useTimeout() {
   const [to, setTo] = useState<NodeJS.Timeout | null>(null);
 
